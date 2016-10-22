a6ac60d05b89fd86d79c743bc55db0a072e3a7cc
Erik Sikich <Erik Sikich>
2016-10-22 15:23:10 -0500

    Fixed typo.

--- examples/Cycle/Cycle.ino
+++ examples/Cycle/Cycle.ino
@@ -1,13 +1,13 @@
 /*
-	Cycle.ino
+  Cycle.ino
 
-	This program cycles through a list of colors
+  This program cycles through a list of colors
 
-	The circuit:
-	1 RGB LED attached to pins 2, 3, and 4
+  The circuit:
+  1 RGB LED attached to pins 2, 3, and 4
 
-	Created 21/10/16
-	By Erik Sikich
+  Created 21/10/16
+  By Erik Sikich
 */
 
 #include "RGBLEDBlender.h"
@@ -15,11 +15,11 @@
 void setup(){}
 
 void loop(){
-	Color color_list[6] = {RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE};
+  Color color_list[6] = {RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE};
     
     RGBLEDBlender my_blender(2, 3, 4);
-	
-    for(uint8_t i = 0; i < 6; i++) my_blender.AddColor(colorList[i]);
+  
+    for(uint8_t i = 0; i < 6; i++) my_blender.AddColor(color_list[i]);
 
     while(true){
         //Fade through list for 1000 ms
